4e2292e17f57e20d6abe1e9b1cbdcbc0b37b035e
JOSEONGIK <JOSEONGIK@HB03-04>
2019-01-04 17:58:05 +0900

    modify

--- src/main/java/org/jj/controller/UploadController.java
+++ src/main/java/org/jj/controller/UploadController.java
@@ -3,14 +3,28 @@ package org.jj.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.jj.domain.AttachFileDTO;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
@@ -21,6 +35,7 @@ import net.coobird.thumbnailator.Thumbnailator;
 
 public class UploadController {
 
+	
 /*	@GetMapping("/uploadForm")
 	public void uploadForm() {
 
@@ -46,6 +61,7 @@ public class UploadController {
 			} // end catch
 		} // end for
 	}*/
+	
 	
 	
 	private boolean checkImageType(File file) {
@@ -78,14 +94,15 @@ public class UploadController {
 	}
 	
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		
-		log.info("update ajax post.................");
-		
+		List<AttachFileDTO> list = new ArrayList<>();		
 		String uploadFolder = "C:\\upload";
 		
-		File uploadPath = new File(uploadFolder, getFolder());
+		String uploadFolderPath = getFolder();
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		
 		if(uploadPath.exists() == false) {
 			uploadPath.mkdirs();
@@ -97,6 +114,7 @@ public class UploadController {
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
 			
+			AttachFileDTO attachDTO = new AttachFileDTO();			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
@@ -106,20 +124,102 @@ public class UploadController {
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			//중복방지를 위한 uuid 적용
 			
+			log.info(uuid);
 			
 			try {
+				log.info(uploadPath);
 				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
 				//check image type file 썸네일처리하기
 				if(checkImageType(saveFile)) {
 					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
 					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();
 				}
+				
+				list.add(attachDTO);
+				log.info(list);
+				
 			} catch (Exception e) {
-				log.error(e.getMessage()); //파일받기
+				e.printStackTrace();
 			} // end catch
-		}
+			
+		}return new ResponseEntity<>(list, HttpStatus.OK);			
+
 	}
+	
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFiles(String fileName){
+		
+		log.info("fileName: " + fileName);
+		
+		File file = new File("c:\\upload\\" + fileName);
+		
+		log.info("file: " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			HttpHeaders header = new HttpHeaders();
+			
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent")String userAgent, String fileName){
+		
+		Resource resource = new FileSystemResource("c:\\upload\\"+fileName);
+		
+		log.info("download file: " + fileName);
+		
+		if(resource.exists() == false) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+				
+		String resourceName = resource.getFilename();
+		
+		HttpHeaders headers = new HttpHeaders();
+		try {
+			
+			String downloadName = null;
+			
+			if( userAgent.contains("Trident")) {
+				log.info("IE browser");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replaceAll("\\+"," ");
+			}else if(userAgent.contains("Edge")) {
+				log.info("Edge browser");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8")
+				
+			}else {
+				
+				log.info("Chrome browser");
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+			
+			log.info("downloadName: " + downloadName);
+			
+			headers.add("Content=Disposition", "attachment; filename=" + downloadName);
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}
+
+	
 	
 }
--- src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -6,6 +6,28 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
+<style>
+.uploadResult {
+	width: 100%;
+	background-color: gray;
+}
+
+.uploadResult ul {
+	display: flex;
+	flex-flow: row;
+	justify-content: center;
+	align-items: center;
+}
+
+.uploadResult ul li {
+	list-style: none;
+	padding: 10px;
+}
+
+.uploadResult ul li img {
+	width: 20px;
+}
+</style>
 <body>
 	<h1>Upload With Ajax</h1>
 
@@ -13,66 +35,91 @@
 		<input type='file' name='uploadFile' multiple>
 	</div>
 
+	<div class='uploadResult'>
+		<ul>
+		</ul>
+	</div>
+
 	<button id='uploadBtn'>Upload</button>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" 
-	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" 
-	crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+		integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+		crossorigin="anonymous"></script>
 
 	<script>
-	var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
-	var maxSize = 1048576;
+		var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+		var maxSize = 1048576;
 
-	function checkExtension(fileName, fileSize){
-		
-		if( fileName >= maxSize){
-			alert("파일 사이즈 초과");
-			return false;
+		function checkExtension(fileName, fileSize) {
+
+			if (fileName >= maxSize) {
+				alert("파일 사이즈 초과");
+				return false;
+			}
+
+			if (regex.test(fileName)) {
+				alert("해당 종류의 파일은 업로드할 수 없습니다.");
+				return false;
+			}
+			return true;
 		}
-		
-		if(regex.test(fileName)){
-			alert("해당 종류의 파일은 업로드할 수 없습니다.");
-			return false;
-		}
-		return true;
-	}
 
-		$(document).ready(function() {
-			
-			$("#uploadBtn").on("click", function(e) {
+		var cloneObj = $(".uploadDiv").clone();
 
-				var formData = new FormData();
+		$("#uploadBtn").on("click", function(e) {
 
-				var inputFile = $
-				(
-					"input[name='uploadFile']"
-				)
-				;
+			var formData = new FormData();
 
-				var files = inputFile[0].files;
-				console.log(files);
+			var inputFile = $("input[name='uploadFile']");
 
-				for (var i = 0; i < files.length; i++) {
-					
-					 if(!checkExtension(files[i].name, files[i].size)){
-							return false;
-						} 
-					 
-					formData.append("uploadFile", files[i]);
+			var files = inputFile[0].files;
+			console.log(files);
+
+			for (var i = 0; i < files.length; i++) {
+
+				if (!checkExtension(files[i].name, files[i].size)) {
+					return false;
 				}
 
-				$.ajax({
-					url : '/uploadAjaxAction',
-					processData : false,
-					contentType : false,
-					data : formData,
-					type : "POST",
-					success : function(result) {
-						alert("Uploaded");
-					}
-				});
+				formData.append("uploadFile", files[i]);
+			}
+
+			$.ajax({
+				url : '/uploadAjaxAction',
+				processData : false,
+				contentType : false,
+				data : formData,
+				type : "POST",
+				success : function(result) {
+					alert("Uploaded");
+					 showUploadedFile(result);
+					$(".uploadDiv").html(cloneObj.html());
+
+				}
 			});
 		});
+
+		var uploadResult = $(".uploadResult ul");
+		
+		function showUploadedFile(uploadResultArr) {
+			var str = "";
+
+			$(uploadResultArr).each(
+					function(i, obj) {
+
+						if (!obj.image) {
+							str += "<li><img src='/resources/img/attach.png'>" + obj.fileName +"</li>";
+						} else {
+							//str += "<li>" + obj.fileName + "</li>";
+							
+							var fileCallPath = encodeURIComponent ( obj.uploadPath+"/ s_"+obj.uuid+"_"+obj.fileName);
+							
+							str += "<li><img src='/dipslay?fileName="+fileCallPath+"'><li>";
+						}
+					});
+
+			uploadResult.append(str);
+		}
 	</script>
 </body>
 </html>
